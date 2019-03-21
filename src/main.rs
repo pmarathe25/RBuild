8c01647fc4b1ca89322a38a1f3d48648df388b1b
Pranav Marathe <pmarathe25@gmail.com>
2019-03-20 18:00:18 -0700

    Migrates to RgParse for command-line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,37 @@
-use std::env;
 use std::string::String;
 use std::time::SystemTime;
 use std::collections::HashMap;
+use rgparse::{Parser, Parameter};
 
 mod builder;
 use builder::build_graph;
 
-fn help(executable: &str) {
-    println!("Usage: {} RBUILD_FILE [TARGETS...]
-    RBUILD_FILE     Path to the rbuild configuration file.
-    TARGETS         Zero or more targets to execute.", executable);
-    std::process::exit(1);
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut parser = Parser::new("A tool for fast incremental builds");
+    parser.add_parameter(Parameter::param("--threads", "The number of threads to use during execution").alias("-t"));
+    let args = parser.parse_args();
 
-    if args.len() < 2 {
-        help(&args[0]);
-    }
-
-    let config = match std::fs::read_to_string(&args[1]) {
-        Err(what) => panic!("Could not open provided configuration file {}: {}", args[1], what),
+    let config_path = args.positional.get(0).expect("No configuration file provided.");
+    let config = match std::fs::read_to_string(&config_path) {
+        Err(what) => panic!("Could not open provided configuration file {}: {}", config_path, what),
         Ok(file) => file,
     };
 
-    // TODO: Number of threads should be configurable. Need RgParser for this.
-    let (mut graph, node_map) = build_graph(&config, 8);
+    let (mut graph, node_map) = build_graph(&config, args.get("--threads").unwrap_or(8));
 
     let mut fetches = Vec::new();
-    for target in args.iter().skip(2) {
-        fetches.push(node_map.get(target).expect(
+    for target in args.positional.into_iter().skip(1) {
+        fetches.push(node_map.get(&target).expect(
             &format!("{} is not a valid target", target)
         ));
     }
 
-    let recipe = graph.compile(fetches);
-    let mut inputs_map = HashMap::with_capacity(recipe.inputs.len());
-    for input in &recipe.inputs {
-        inputs_map.insert(input.clone(), vec![SystemTime::UNIX_EPOCH]);
+    if fetches.len() > 0 {
+        let recipe = graph.compile(fetches);
+        let mut inputs_map = HashMap::with_capacity(recipe.inputs.len());
+        for input in &recipe.inputs {
+            inputs_map.insert(input.clone(), vec![SystemTime::UNIX_EPOCH]);
+        }
+        graph.run(&recipe, inputs_map);
     }
-    graph.run(&recipe, inputs_map);
 }
